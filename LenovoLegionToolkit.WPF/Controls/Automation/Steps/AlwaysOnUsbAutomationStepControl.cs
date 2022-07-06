1d56005a4996dd18dea119024b40f9a1c0a6cf7c
Cryoshell <37774657+Cryoshell99@users.noreply.github.com>
2022-07-06 15:31:26 +0700

    RU

--- LenovoLegionToolkit.WPF/Controls/Automation/Steps/AlwaysOnUsbAutomationStepControl.cs
+++ LenovoLegionToolkit.WPF/Controls/Automation/Steps/AlwaysOnUsbAutomationStepControl.cs
@@ -9,8 +9,8 @@ namespace LenovoLegionToolkit.WPF.Controls.Automation.Steps
         public AlwaysOnUsbAutomationStepControl(IAutomationStep<AlwaysOnUSBState> step) : base(step)
         {
             Icon = SymbolRegular.UsbStick24;
-            Title = "Always on USB";
-            Subtitle = "Charge USB devices, when the computer is\noff or in sleep or hibernation mode.";
+            Title = "USB всегда включен";
+            Subtitle = "Заряжайте USB-устройства, когда компьютер\nнаходится в режиме сна, гибернации или выключен.";
         }
     }
 }
